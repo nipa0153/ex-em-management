d99dc9ab7913912e9834f9e8e930330e5ef1ac1d
Shigeaki.Sakaida <shigeaki.sakaida@gmail.com>
2023-11-08 17:09:14 +0900

    WIP: admin repository finished

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -1,9 +1,13 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Administrator;
@@ -30,9 +34,40 @@ public class AdministratorRepository {
         return administrator;
     };
 
-    public void insert(){
+    public static final String SQL_ADMIN_SELECT = """
+                        SELECT 
+                            mail_address, 
+                            password 
+                        FROM 
+                            administrators;
+                        """;
+    
+    public static final String SQL_INSERT = """
+                        INSERT INTO administrators(
+                            name,
+                            mailAddress,
+                            password
+                            )
+                        VALUES(
+                            :name,
+                            :mainAddress,
+                            :password
+                            );
+                        """;
+
+    public void insert(Administrator administrator){
+        SqlParameterSource param  = new BeanPropertySqlParameterSource(administrator);
+        template.update(SQL_INSERT, param);
 
     }
 
-
+    public List<Administrator> findByMailAddressAndPassword(String mailAddress, String password){
+        SqlParameterSource param = new MapSqlParameterSource().addValue(mailAddress, password);
+        List<Administrator> administratorList = template.query(SQL_ADMIN_SELECT, ADMIN_ROW_MAPPER);
+    if (administratorList.size() == 0) {
+        return null;
+    }
+    return administratorList.get(0);
+    
+    }
 }
