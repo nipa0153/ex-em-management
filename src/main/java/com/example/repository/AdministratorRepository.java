80019e1881bf4eb2f1ee6ab526ae450a5318a561
Shigeaki.Sakaida <shigeaki.sakaida@gmail.com>
2023-11-08 16:18:48 +0900

    WIP: admin repository1

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -1,8 +1,38 @@
 package com.example.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Administrator;
+
+
 /**
  * 管理者情報を操作するリポジトリ.
+ * 
  */
+
+@Repository 
 public class AdministratorRepository {
-    
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Administrator> ADMIN_ROW_MAPPER 
+    = (rs, i) -> {
+        Administrator administrator = new Administrator();
+        administrator.setId(rs.getInt("id"));
+        administrator.setName(rs.getString("name"));
+        administrator.setMailAddress(rs.getString("mail_address"));
+        administrator.setPassword(rs.getString("password"));
+
+        return administrator;
+    };
+
+    public void insert(){
+
+    }
+
+
 }
