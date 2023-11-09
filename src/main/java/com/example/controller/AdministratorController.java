5c88dbcd7d3dfe4691e3df451f794b0a73dbb246
Shigeaki.Sakaida <shigeaki.sakaida@gmail.com>
2023-11-09 12:03:48 +0900

    finished Insert.html

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,28 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+
 /**
  * Administrator
  * 管理者情報を表すドメイン.
  * 
  */
+@Controller
+@RequestMapping("/")
 public class AdministratorController {
     
+    @Autowired
+    private AdministratorService administratorService;
+
+    @GetMapping("/toInsert")
+    public String toInsert(InsertAdministratorForm form){
+        return "administrator/insert.html";
+    }
+
 }
